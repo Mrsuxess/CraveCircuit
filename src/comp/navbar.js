e631ca7f89eac23db921a6ed86adb76af9f2772e
Mrsuxess <habeebabdulwasiu144@gmail.com>
2023-10-19 11:53:50 -0700

    edition and authentication

--- src/comp/navbar.js
+++ src/comp/navbar.js
@@ -1,19 +1,28 @@
 import { Link } from "react-router-dom";
 import { auth } from "../config/firebase";
 import { useAuthState} from "react-firebase-hooks/auth"
+import {signOut} from "firebase/auth"
  export const Navbar = () => {
     const[user] = useAuthState(auth);
+
+    const signUserOut = async () => {
+        await signOut(auth);
+
+    }
     return (
     <div className="topnav">
         <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Contact us</Link>
         <Link to="/apis">Meals</Link>
-        <Link to="/About">About us</Link>
+
         <div>
             <div className="user">
+                {user && (
+                <>
             <p>{user?.displayName}</p>
             <img src={user?.photoURL || ""} width="20" height="20"  alt=""/>
+            <button onClick={signUserOut}>log out</button>
+            </>
+                )}
         </div>
         </div>
         </div>
--- src/component/main.js
+++ src/component/main.js
@@ -1,5 +1,22 @@
-export const Main = () => {
+import {auth, provider} from "../config/firebase";
+import {signInWithPopup} from "firebase/auth";
+import { useNavigate} from "react-router-dom"
 
-    return <div><h1>Home</h1></div>
+export const Main = () => {
+    const navigate = useNavigate();
+    const signInWithGoogle = async () => {
+        const result = await signInWithPopup(auth, provider);
+        console.log(result);
+        navigate("/apis");
+    };
+       
+    
+
+    
+    return <div>
+        <h2>To Continue</h2> 
+        <h4>Sign In With Google To Continue</h4>
+    <button onClick={ signInWithGoogle }><b>Sign in with Google</b></button>
+    </div>
 
 };
